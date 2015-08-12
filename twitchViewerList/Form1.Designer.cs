a750110a367a634102631aad0fd0a684b68e79f6
jmalish <jordanmalish@gmail.com>
2015-08-12 10:34:04 -0400

    first commit

---
+++ twitchViewerList/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblViewCount = new System.Windows.Forms.Label();
+            this.lblChannel = new System.Windows.Forms.Label();
+            this.lstViewerList = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.changeChannelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewerIconsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sStaffToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aAdminToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gMGlobalModsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 15000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblViewCount
+            // 
+            this.lblViewCount.AutoSize = true;
+            this.lblViewCount.Location = new System.Drawing.Point(12, 24);
+            this.lblViewCount.Name = "lblViewCount";
+            this.lblViewCount.Size = new System.Drawing.Size(56, 13);
+            this.lblViewCount.TabIndex = 0;
+            this.lblViewCount.Text = "Viewers: 0";
+            // 
+            // lblChannel
+            // 
+            this.lblChannel.AutoSize = true;
+            this.lblChannel.Location = new System.Drawing.Point(98, 24);
+            this.lblChannel.Name = "lblChannel";
+            this.lblChannel.Size = new System.Drawing.Size(52, 13);
+            this.lblChannel.TabIndex = 0;
+            this.lblChannel.Text = "Channel: ";
+            // 
+            // lstViewerList
+            // 
+            this.lstViewerList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstViewerList.FormattingEnabled = true;
+            this.lstViewerList.Location = new System.Drawing.Point(12, 40);
+            this.lstViewerList.Name = "lstViewerList";
+            this.lstViewerList.Size = new System.Drawing.Size(217, 316);
+            this.lstViewerList.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(241, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.changeChannelToolStripMenuItem});
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // changeChannelToolStripMenuItem
+            // 
+            this.changeChannelToolStripMenuItem.Name = "changeChannelToolStripMenuItem";
+            this.changeChannelToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.changeChannelToolStripMenuItem.Text = "Change Channel";
+            this.changeChannelToolStripMenuItem.Click += new System.EventHandler(this.changeChannelToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewerIconsToolStripMenuItem,
+            this.sStaffToolStripMenuItem,
+            this.aAdminToolStripMenuItem,
+            this.gMGlobalModsToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(99, 20);
+            this.helpToolStripMenuItem.Text = "Icon Dictionary";
+            // 
+            // viewerIconsToolStripMenuItem
+            // 
+            this.viewerIconsToolStripMenuItem.Name = "viewerIconsToolStripMenuItem";
+            this.viewerIconsToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.viewerIconsToolStripMenuItem.Text = "@ - Moderators";
+            // 
+            // sStaffToolStripMenuItem
+            // 
+            this.sStaffToolStripMenuItem.Name = "sStaffToolStripMenuItem";
+            this.sStaffToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.sStaffToolStripMenuItem.Text = "S_ - Staff";
+            // 
+            // aAdminToolStripMenuItem
+            // 
+            this.aAdminToolStripMenuItem.Name = "aAdminToolStripMenuItem";
+            this.aAdminToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.aAdminToolStripMenuItem.Text = "A_ - Admin";
+            // 
+            // gMGlobalModsToolStripMenuItem
+            // 
+            this.gMGlobalModsToolStripMenuItem.Name = "gMGlobalModsToolStripMenuItem";
+            this.gMGlobalModsToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.gMGlobalModsToolStripMenuItem.Text = "GM_ - Global Mods";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(241, 365);
+            this.Controls.Add(this.lstViewerList);
+            this.Controls.Add(this.lblChannel);
+            this.Controls.Add(this.lblViewCount);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.Text = "Twitch Viewer List";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblViewCount;
+        private System.Windows.Forms.Label lblChannel;
+        private System.Windows.Forms.ListBox lstViewerList;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem changeChannelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewerIconsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sStaffToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aAdminToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gMGlobalModsToolStripMenuItem;
+    }
+}
+
---
+++ twitchViewerList/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Collections;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Microsoft.VisualBasic;
+
+namespace WindowsFormsApplication1
+{
+    public partial class frmMain : Form
+    {
+        // variables
+        public string channelName, chatterURL, jsonString;
+        int viewCount = 0;
+        public dynamic jObj;
+        public WebClient wClient = new WebClient();
+        JArray modList, staffList, adminList, globalModList, viewerList;
+        Properties.Settings settings = Properties.Settings.Default;
+
+
+        public frmMain()
+        {
+            InitializeComponent();   
+        }
+
+        // on form load
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // if channel is not set, prompt user for new channel
+            if (settings.channelName == null)
+            {
+                changeChannel();
+            }
+            else
+            {
+                channelName = settings.channelName;
+            }
+
+            // put channel into api link
+            chatterURL = "http://tmi.twitch.tv/group/user/" + channelName.ToLower() + "/chatters";
+
+            // update label
+            lblChannel.Text = ("Channel: " + channelName);
+                        
+            updateInfo();
+        }
+
+        // everytime timer ticks (15 sec default)
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // call updateInfo()
+            updateInfo();
+        }
+
+        // updates viewer count and user list
+        public void updateInfo()
+        {
+            try
+            {
+                jsonString = wClient.DownloadString(chatterURL);
+                jObj = JsonConvert.DeserializeObject(jsonString);
+
+                viewCount = jObj["chatter_count"];
+
+                modList = jObj["chatters"]["moderators"];
+                staffList = jObj["chatters"]["staff"];
+                adminList = jObj["chatters"]["admins"];
+                globalModList = jObj["chatters"]["global_mods"];
+                viewerList = jObj["chatters"]["viewers"];
+
+                lstViewerList.Items.Clear();
+
+                lblViewCount.Text = ("Viewers: " + viewCount.ToString());
+
+                foreach (string m in modList)
+                {
+                    lstViewerList.Items.Add("@" + m);
+                }
+
+                foreach (string s in staffList)
+                {
+                    lstViewerList.Items.Add("S_" + s);
+                }
+
+                foreach (string a in adminList)
+                {
+                    lstViewerList.Items.Add("A_" + a);
+                }
+
+                foreach (string gm in globalModList)
+                {
+                    lstViewerList.Items.Add("GM_" + gm);
+                }
+
+                foreach (string v in viewerList)
+                {
+                    lstViewerList.Items.Add(v);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message.ToString() +
+                    "\nStopping program. Use the menu strip on the main window to change channel name.");
+
+                timer1.Enabled = false;
+            }
+        }
+
+        private void changeChannelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            changeChannel();
+        }
+
+        private void changeChannel()
+        {
+            timer1.Enabled = false;
+
+            string input = Microsoft.VisualBasic.Interaction.InputBox("Enter new channel below:", "Enter new channel");
+
+            if (input.Length > 0)
+            {
+                channelName = input;
+                settings.channelName = channelName;
+                settings.Save();
+
+                chatterURL = "http://tmi.twitch.tv/group/user/" + channelName.ToLower() + "/chatters";
+
+                lblChannel.Text = ("Channel: " + channelName);
+            }
+            else
+            {
+                // do nothing
+            }
+
+            timer1.Enabled = true;
+            updateInfo();
+        }
+    }
+}
